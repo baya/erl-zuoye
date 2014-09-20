fef3cf132e1195d0128679bba5e2c5f6089afab6
baya <kayak.jiang@gmail.com>
2014-09-20 22:23:08 +0800

    update

--- zuoye74.erl
+++ zuoye74.erl
@@ -1,5 +1,6 @@
 -module(zuoye74).
--export([reverse_binary/1,get_reverse_end_order/0]).
+-export([reverse_binary/1,get_reverse_end_order/0, reverse_bits/1]).
+-export([test/0]).
 
 reverse_binary(Bin) ->
     Size = byte_size(Bin) * 8,
@@ -9,11 +10,26 @@ reverse_binary(Bin) ->
 	big -> <<Value:Size/big>>
     end.
 
-
 get_reverse_end_order() ->
     <<Value:16>> = <<16#123:16>>,
     case <<Value:16>> of
 	<<Value:16/big>> -> little;
 	<<Value:16/little>> -> big
     end.
+
+
+reverse_bits(Bin) ->
+    Bits = [X || <<X:1>> <= Bin],
+    << <<X>> || X <- my_reverse_list(Bits)>>.
+
+my_reverse_list([]) -> [];
+my_reverse_list([H|T]) ->
+    my_reverse_list(T) ++ [H].
+
+
+
+test() ->
+    [1,2,3] = my_reverse_list([3,2,1]),
+    <<1,1,1,1,0,0,0,0>> = reverse_bits(<<16#f>>).
+
 
