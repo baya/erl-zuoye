08f39483633c77d42da625ba38d4d2b239bf4d90
baya <kayak.jiang@gmail.com>
2014-10-04 22:43:51 +0800

    update

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,5 +1,7 @@
 -module(lib_misc).
 -export([for/3,qsort/1,pythag/1,perms/1,on_exit/2]).
+-export([unconsult/2]).
+
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I) | for(I+1, Max, F)].
@@ -36,3 +38,11 @@ on_exit(Pid, Fun) ->
 keep_alive(Name, Fun) ->
     register(Name, Pid = spawn(Fun)),
     on_exit(Pid, fun(_Why) -> keep_alive(Name, Fun) end).
+
+
+unconsult(File, L) ->
+    {ok, S} = file:open(File, write),
+    lists:foreach(fun(X) -> io:format(S, "~p.~n", [X]) end, L),
+    file:close(S).
+
+
